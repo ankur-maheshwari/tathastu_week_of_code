14604c88d74480e68b4999c3760547fc820578f0
Ankur Maheshwari <65300192+ankur-maheshwari@users.noreply.github.com>
2020-05-22 17:13:32 +0530

    Create program1.py

---
+++ day1/program1.py
@@ -0,0 +1,10 @@
+Name=input("Enter ur name : ")    # enter name  
+Age=input("Enter ur  Age : ")      # enter agae
+Gender=input("Enter ur  gender : ")    # enter gender
+College=input("Enter ur  College name : ")    # enter college name
+Branch=input("Enter ur branch : ")    # enter branch 
+print("Name: ", Name)
+print("Age: ", Age)
+print("Gender: ", Gender)
+print("College: ", College)
+print("Branch: ", Branch)
